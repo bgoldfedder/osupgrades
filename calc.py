a09129a29592037f08f0162c131971179c0308ae
Tony Nelson <tnelson@starpoint.com>
2017-05-06 12:21:59 -0400

    Corrected the selection of the next state

--- calc.py
+++ calc.py
@@ -56,10 +56,12 @@ def briefcase_bonus():
 
 
 def create_state(thermos_count=0, briefcase_count=0,
-                 thermos_upgrades=[], briefcase_upgrades=[]):
+                 thermos_upgrades=[], briefcase_upgrades=[],
+                 curr_bonus=0):
     """This method creates a state tuple from the given inputs"""
     return (thermos_count, briefcase_count,
-            thermos_upgrades, briefcase_upgrades)
+            thermos_upgrades, briefcase_upgrades,
+            curr_bonus)
 
 
 def thermos_count(state):
@@ -72,8 +74,15 @@ def briefcase_count(state):
     return state[1]
 
 
+def current_bonus(state):
+    """This method returns the bonus of the given state"""
+    return state[4]
+
+
 def calc_bonus(thermos_count, briefcase_count,
                thermos_upgrades, briefcase_upgrades):
+
+    # print("{},{}".format(thermos_count, briefcase_count))
 
     if (thermos_count < 0):
         return 0
@@ -85,35 +94,49 @@ def calc_bonus(thermos_count, briefcase_count,
 def next_state(state):
     """This method determines the next best state, given the input state"""
     (thermos_count, briefcase_count,
-        thermos_upgrades, briefcase_upgrades) = state
-
-    new_thermos_bonus = calc_bonus(thermos_count + 1, briefcase_count,
-                                   thermos_upgrades, briefcase_upgrades)
+        thermos_upgrades, briefcase_upgrades, curr_bonus) = state
 
     new_briefcase_bonus = calc_bonus(thermos_count - 8, briefcase_count + 1,
                                      thermos_upgrades, briefcase_upgrades)
 
-    # print("thermos: {} briefcase: {}".format(
-    #    math.trunc(new_thermos_bonus * 100),
-    #    math.trunc(new_briefcase_bonus * 100)))
+    new_thermos_bonus = calc_bonus(thermos_count + 1, briefcase_count,
+                                   thermos_upgrades, briefcase_upgrades)
 
-    if (new_thermos_bonus > new_briefcase_bonus):
+    # print("{}, {}, curr: {:.2f} thermos: {:.2f} briefcase: {:.2f}".format(
+    #     thermos_count, briefcase_count, curr_bonus * 100,
+    #     new_thermos_bonus * 100,
+    #     new_briefcase_bonus * 100))
+
+    if (new_briefcase_bonus <= curr_bonus and
+            new_thermos_bonus > new_briefcase_bonus):
         return next_state(create_state(thermos_count + 1, briefcase_count,
-                                       thermos_upgrades, briefcase_upgrades))
+                                       thermos_upgrades, briefcase_upgrades,
+                                       new_thermos_bonus))
     else:
         return create_state(thermos_count - 8, briefcase_count + 1,
-                            thermos_upgrades, briefcase_upgrades)
+                            thermos_upgrades, briefcase_upgrades, 
+                            new_briefcase_bonus)
 
 
 def main(options, args):
     """main loop of the progam"""
+
+    # for i in range(38, 50):
+    #     print("{},{},{:.2f},{},{},{:.2f}".format(
+    #           i, 8, calc_bonus(i, 8, [], []) * 100,
+    #           i - 8, 9, calc_bonus(i - 8, 9, [], []) * 100))
+
+    # sys.exit(0)
 
     state = create_state()
 
     while (thermos_count(state) < options.max):
         state = next_state(state)
         if (thermos_count(state) < options.max):
-            print(state)
+            print("{:6d},{:6d} => {:.2f}".format(
+                thermos_count(state), briefcase_count(state),
+                round(calc_bonus(thermos_count(state), briefcase_count(state),
+                                 [], []) * 100, 2)))
 
 
 if __name__ == '__main__':
